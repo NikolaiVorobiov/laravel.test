d7deb8e3a6fd81afab7d029ded1faae1eeb138b1
mykola.vorobiov <nikolay.w77@gmail.com>
2023-08-24 15:03:50 +0300

    Added saving user's google data to DB

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Auth;
 use Exception;
 use App\Models\User;
-use App\Models\Team;
+use Illuminate\Support\Str;
 
 class LoginController extends Controller
 {
@@ -29,50 +30,17 @@ class LoginController extends Controller
      */
     public function handleGoogleCallback(Request $request)
     {
+        $googleUser = Socialite::driver('google')->stateless()->user();
+        User::firstOrCreate(
+            ['email' => $googleUser->user['email']],
+            [
+                'password' => Hash::make(Str::random(10)),
+                'first_name' => $googleUser->user['given_name'],
+                'last_name' => $googleUser->user['family_name'],
+            ]
+        );
+
         $request->session()->put('admin', true);
         return view('admin');
-
-//        try {
-//            //create a user using socialite driver google
-//            $user = Socialite::driver('google')->user();
-//            // if the user exits, use that user and login
-//            $finduser = User::where('google_id', $user->id)->first();
-//
-//
-//            if ($finduser) {
-//                //if the user exists, login and show dashboard
-//                Auth::login($finduser);
-//                return redirect('/dashboard');
-//            } else {
-//                //user is not yet created, so create first
-//                $newUser = User::create([
-//                    'name' => $user->name,
-//                    'email' => $user->email,
-//                    'google_id'=> $user->id,
-//                    'password' => encrypt('')
-//                ]);
-//                //every user needs a team for dashboard/jetstream to work.
-//                //create a personal team for the user
-//                $newTeam = Team::forceCreate([
-//                    'user_id' => $newUser->id,
-//                    'name' => explode(' ', $user->name, 2)[0]."'s Team",
-//                    'personal_team' => true,
-//                ]);
-//                // save the team and add the team to the user.
-//                $newTeam->save();
-//                $newUser->current_team_id = $newTeam->id;
-//                $newUser->save();
-//                //login as the new user
-//                Auth::login($newUser);
-//                // go to the dashboard
-//                return redirect('/dashboard');
-//            }
-//            //catch exceptions
-//        } catch (Exception $e) {
-//            dd($e->getMessage());
-//        }
-
-//        $user = Socialite::driver('google')->user();
-
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['email', 'password', 'first_name', 'last_name'];
 }
